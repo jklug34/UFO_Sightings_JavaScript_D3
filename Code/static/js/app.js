867c046d4649ff1f49061ea60ebc13b9e26a4d21
Mil34 <jasonrklug@gmail.com>
2019-07-27 19:57:18 -0700

    Updated files and updated readme

--- Code/static/js/app.js
+++ Code/static/js/app.js
@@ -1,53 +1,67 @@
-// * Create a basic HTML web page or use the [index.html](StarterCode/index.html) file provided (we recommend building your own custom page!).
-
-// * Using the UFO dataset provided in the form of an array of JavaScript objects, write code that appends a table to your web page and then adds new rows of data for each UFO sighting.
-
-// * Make sure you have a column for `date/time`, `city`, `state`, `country`, `shape`, and `comment` at the very least.
-
-// * Use a date form in your HTML document and write JavaScript code that will listen for events and search through the `date/time` column to find rows that match user input.
-
 
 // from data.js assign a varible
 var tableData = data;
 console.log(tableData);
 
+
 // select the filter table button
-var button = d3.selectAll(".btn-default");
+var button = d3.select(".btn-default");
 button.on("click", function() {
-console.log(button)    
+console.log(button)  
+ 
 
     // DATE
     // select the datetime input element and get input value
     if (inputElementDate != "") {
+            d3.event.preventDefault();
+            var inputElementDate = d3.select("#datetime.form-control").node().value  
+            console.log(inputElementDate)
+            // clear the input value
+            d3.select("#datetime.form-control").node().value = "";
+
+            // filter by date in input
+            var filteredDate = tableData.filter(date => date.datetime === inputElementDate);
+            console.log(filteredDate);
+            // console.log(Object.values(filteredDate[0])[0])
+
+            // create seperate arrays with all columns of data from the filtered table by date
+            var dataFilterDate = filteredDate.map(date => date.datetime);
+            var dataFilterCity = filteredDate.map(date => date.city);
+            var dataFilterState = filteredDate.map(date => date.state);
+            var dataFilterCountry = filteredDate.map(date => date.country);
+            var dataFilterShape = filteredDate.map(date => date.shape);
+            var dataFilterDuration = filteredDate.map(date => date.durationMinutes);
+            var dataFilterComments = filteredDate.map(date => date.comments);
+
+            var cleanComments = []
+            var mapObj = {
+            "&#44":",",
+            "&#39":"'",
+            "&#33":"!",
+            "&quot;": "''"
+            };
+            for (var i=0; i<dataFilterComments.length;i++) {
+                    var clean =  dataFilterComments[i].replace(/&#44|&#39|&#33|&quot;/gi, function(matched){
+                    return mapObj[matched]
+                    
+                    });
+                    cleanComments.push(clean)
+            };
         
-        d3.event.preventDefault();
-        var inputElementDate = d3.select("#datetime.form-control").node().value  
-        console.log(inputElementDate)
-         // clear the input value
-        d3.select("#datetime.form-control").node().value = "";
+        
+            console.log(cleanComments);
+            console.log("-------------------------------")
+    return buildTable()  
+    }
+     
 
-        // filter by date in input
-        var filteredDate = tableData.filter(date => date.datetime === inputElementDate);
-        console.log(filteredDate);
-        // console.log(Object.values(filteredDate[0])[0])
-
-        // create seperate arrays with all columns of data from the filtered table by date
-        var dataFilterDate = filteredDate.map(date => date.datetime);
-        var dataFilterCity = filteredDate.map(date => date.city);
-        var dataFilterState = filteredDate.map(date => date.state);
-        var dataFilterCountry = filteredDate.map(date => date.country);
-        var dataFilterShape = filteredDate.map(date => date.shape);
-        var dataFilterDuration = filteredDate.map(date => date.durationMinutes);
-        var dataFilterComments = filteredDate.map(date => date.comments);
-        }
-
-    // CITY
-    // select the city input element and get input value
-    else if (inputElementCity != "") {
+        // CITY
+        // select the city input element and get input value
+    if (inputElementCity != "") {
 
         d3.event.preventDefault();
         var inputElementCity = d3.select("#city.form-control").node().value;
-         // clear the input value
+        // clear the input value
         d3.select("#city.form-control").node().value = "";  
 
         // filter by city input
@@ -62,15 +76,35 @@ console.log(button)
         var dataFilterShape = filteredCity.map(city => city.shape);
         var dataFilterDuration = filteredCity.map(city => city.durationMinutes);
         var dataFilterComments = filteredCity.map(city => city.comments);
-        }
 
-    //STATE
-    // select the state input element and get input value
-    else if (inputElementState != "") {    
+        var cleanComments = []
+        var mapObj = {
+        "&#44":",",
+        "&#39":"'",
+        "&#33":"!",
+        "&quot;": "''"
+        };
+        for (var i=0; i<dataFilterComments.length;i++) {
+                var clean =  dataFilterComments[i].replace(/&#44|&#39|&#33|&quot;/gi, function(matched){
+                return mapObj[matched]
+                
+                });
+                cleanComments.push(clean)
+        };
+    
+    
+        console.log(cleanComments);
+        console.log("-------------------------------")
+    return buildTable()  
+    }
+
+        //STATE
+        // select the state input element and get input value
+    if (inputElementState != "") {    
 
         d3.event.preventDefault();
         var inputElementState = d3.select("#state.form-control").node().value;
-         // clear the input value
+        // clear the input value
         d3.select("#state.form-control").node().value = "";  
 
         // filter by state input
@@ -85,15 +119,35 @@ console.log(button)
         var dataFilterShape = filteredState.map(state => state.shape);
         var dataFilterDuration = filteredState.map(state => state.durationMinutes);
         var dataFilterComments = filteredState.map(state => state.comments);
-        }
+
+        var cleanComments = []
+        var mapObj = {
+        "&#44":",",
+        "&#39":"'",
+        "&#33":"!",
+        "&quot;": "''"
+        };
+        for (var i=0; i<dataFilterComments.length;i++) {
+                var clean =  dataFilterComments[i].replace(/&#44|&#39|&#33|&quot;/gi, function(matched){
+                return mapObj[matched]
+                
+                });
+                cleanComments.push(clean)
+        };
     
-    // COUNTRY
-    // select the country input element and get input value
-    else if (inputElementCountry != "") {      
+    
+        console.log(cleanComments);
+        console.log("-------------------------------")
+    return buildTable() 
+    }
+
+        // COUNTRY
+        // select the country input element and get input value
+    if (inputElementCountry != "") {      
 
         d3.event.preventDefault();
         var inputElementCountry = d3.select("#country.form-control").node().value;
-         // clear the input value
+        // clear the input value
         d3.select("#country.form-control").node().value = "";  
 
         // filter by country input
@@ -108,15 +162,35 @@ console.log(button)
         var dataFilterShape = filteredCountry.map(country => country.shape);
         var dataFilterDuration = filteredCountry.map(country => country.durationMinutes);
         var dataFilterComments = filteredCountry.map(country => country.comments);
-        }
-    
-    // SHAPE
-    // select the shape input element and get input value
-    else if (inputElementShape != "") {     
+
+        var cleanComments = []
+        var mapObj = {
+        "&#44":",",
+        "&#39":"'",
+        "&#33":"!",
+        "&quot;": "''"
+        };
+        for (var i=0; i<dataFilterComments.length;i++) {
+                var clean =  dataFilterComments[i].replace(/&#44|&#39|&#33|&quot;/gi, function(matched){
+                return mapObj[matched]
+                
+                });
+                cleanComments.push(clean)
+        };
+                        
+                        
+        console.log(cleanComments);
+        console.log("-------------------------------")
+    return buildTable() 
+    }
+                        
+        // SHAPE
+        // select the shape input element and get input value
+    if (inputElementShape != "") {     
 
         d3.event.preventDefault();
         var inputElementShape = d3.select("#shape.form-control").node().value;
-         // clear the input value
+        // clear the input value
         d3.select("#shape.form-control").node().value = "";   
 
         // filter by shape input
@@ -131,38 +205,27 @@ console.log(button)
         var dataFilterShape = filteredShape.map(shape => shape.shape);
         var dataFilterDuration = filteredShape.map(shape => shape.durationMinutes);
         var dataFilterComments = filteredShape.map(shape => shape.comments);
-    }
-    else {
-        console.log("Data not found. Try another search term")
-    }
 
-        // console.log("------------------------------")
-        // console.log(Object.values(filteredDate[0])[0])
-        // console.log("------------------------------")
+        var cleanComments = []
+        var mapObj = {
+        "&#44":",",
+        "&#39":"'",
+        "&#33":"!",
+        "&quot;": "''"
+        };
+        for (var i=0; i<dataFilterComments.length;i++) {
+                var clean =  dataFilterComments[i].replace(/&#44|&#39|&#33|&quot;/gi, function(matched){
+                return mapObj[matched]
+                
+                });
+                cleanComments.push(clean)
+        };
     
-
-    console.log(dataFilterComments);
-
-    // use .replace to replace "&#44", "&#39", "&#33" with ",", "'", "!"  in the comments adding the correct punctuation
-    var cleanComments = []
-    var mapObj = {
-    "&#44":",",
-    "&#39":"'",
-    "&#33":"!",
-    "&quot;": "''"
-    };
-    for (var i=0; i<dataFilterComments.length;i++) {
-            var clean =  dataFilterComments[i].replace(/&#44|&#39|&#33|&quot;/gi, function(matched){
-            return mapObj[matched]
-            
-            });
-            cleanComments.push(clean)
-    };
-
-
-    console.log(cleanComments);
-    console.log("-------------------------------")
-
+    
+        console.log(cleanComments);
+        console.log("-------------------------------")
+    return buildTable() 
+    }
 
     // Build a function that adds rows to the tbody of table id = "ufo-table"
     function buildTable() {
@@ -185,12 +248,4 @@ console.log(button)
         trow.append("td").text(cleanComments[i]);
         };
     };
-
-    // call the function  
-    buildTable()
-    // }       
 });
-
-
-
-
--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # UFO_Sightings_JavaScript_D3
 Create dynamic code in D3.js to search multiple inputs (date/time, city, state, country, shape) in UFO sightings data and outputs the results to a custom web page
+
+
+![spaceship_real](https://user-images.githubusercontent.com/48166327/62001507-ea17d180-b0a6-11e9-9650-f115785b06c3.jpg)
+
+
+## Files:
+- index.html
+- static folder (js: app.js, data.js; css: style.css; image folder)  
+
+
+## To Use:
+
+- Open index.html in google chrome
+- Search by date (1/1/2010 - 1/13/2010) or city or state abbr. or country or shape
+- Click button to retrieve filtered data
+- Form will reset with each new search
+
+
+# Screen Shot:
+
+![Webpage Output](https://user-images.githubusercontent.com/48166327/62001503-e08e6980-b0a6-11e9-9bf5-747520221253.PNG)
+
+![Webpage Output(2)](https://user-images.githubusercontent.com/48166327/62001504-e2582d00-b0a6-11e9-9779-864afe8676bc.PNG)
+
+
+
+
+
+
